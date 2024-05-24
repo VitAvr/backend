9d6ce8b56511e79f1e99dec4992a893e2b3c029a
VitAvr <131783204+VitAvr@users.noreply.github.com>
2024-05-24 08:38:35 +0300

    Update DetailFirm.js

--- src/pages/DetailFirm.js
+++ src/pages/DetailFirm.js
@@ -46,27 +46,27 @@ export default function DetailFirm() {
     return (
         <Container className="mt-1">
         { /*Firm.name */ }
-            <h2 className="text-center m-4"> Фирма "{firm.name}"</h2>
+            <h2 className="text-center m-4"> Фирма "{data.name}"</h2>
         { /*Firm.id */ }
-            <Row className="m-2" key={firm.id}>
+            <Row className="m-2" key={data.id}>
                 { /*Firm.logoUrl */ }
                 <Col md="3">
                     <img className="mr-3 img-thumbnail" 
-                    src={firm.logoUrl || ''} 
+                    src={data.logoUrl || ''} 
                     alt="Логотип" />
                 </Col>
                 <Col md="9">
                         { /*Firm.description */ }
-                        <p>{firm.description}</p>
+                        <p>{data.description}</p>
                         { /*Firm.link */ }
                         <p>
                             <span className="fst-italic">Ссылка на сайт: </span> 
-                            <a href={firm.link || '#'} target='_blank' rel="noopener noreferrer">{firm.link}</a>                            
+                            <a href={data.link || '#'} target='_blank' rel="noopener noreferrer">{data.link}</a>                            
                         </p>
                             { /*Firm.cities */ }
-                        <p>Города: {firm.cities}</p>
+                        <p>Города: {data.cities}</p>
                             { /*Firm.languages */ }
-                        <p>Изучаемые языки: {firm.languages}</p>                        
+                        <p>Изучаемые языки: {data.languages}</p>                        
                     <Link to={`/firms`} className="me-1">
                         Вернуться к списку фирм
                     </Link>
