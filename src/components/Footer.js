16c0a7addb0796ad7f2361ed440a5e720ec58d11
VitAvr <131783204+VitAvr@users.noreply.github.com>
2024-05-24 09:50:31 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
-import React, {Component} from "react";
-import { Container } from "react-bootstrap";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap';
 
-export default class Footer extends Component {
+class Footer extends React.Component {
     render() {
         return (
-            <footer className="footer mt-auto pt-3">
-                <Container
-                className="text-center"
-                fluid
+            <footer
                 style={{
                     backgroundColor: '#bb240e',
                     color: '#fff',
@@ -15,11 +13,41 @@ export default class Footer extends Component {
                     position: 'relative',
                     marginTop: '60px',
                     padding: '10px',
+                    textAlign: 'center',
+                    width: '100%'
                 }}
-                >
+            >
+                <Container fluid>
                     <p>Design &copy; 2024 | LFA | Vitalii Avramenko | JKTV22</p>
                 </Container>
             </footer>
         );
     }
 }
+
+function App() {
+    const appStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh'
+    };
+
+    const contentStyle = {
+        flex: '1'
+    };
+
+    return (
+        <div style={appStyle}>
+            <div style={contentStyle}>
+                {/* Ваше основное содержимое здесь */}
+                <Container className="mt-5">
+                    <h1>Добро пожаловать на наш сайт!</h1>
+                    <p>Основное содержимое страницы.</p>
+                </Container>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
