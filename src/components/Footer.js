08fb377ddb0ca3b5e04d67897073077893dddbf4
VitAvr <131783204+VitAvr@users.noreply.github.com>
2024-05-24 09:52:51 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,30 +24,3 @@ class Footer extends React.Component {
         );
     }
 }
-
-function App() {
-    const appStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh'
-    };
-
-    const contentStyle = {
-        flex: '1'
-    };
-
-    return (
-        <div style={appStyle}>
-            <div style={contentStyle}>
-                {/* Ваше основное содержимое здесь */}
-                <Container className="mt-5">
-                    <h1>Добро пожаловать на наш сайт!</h1>
-                    <p>Основное содержимое страницы.</p>
-                </Container>
-            </div>
-            <Footer />
-        </div>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
